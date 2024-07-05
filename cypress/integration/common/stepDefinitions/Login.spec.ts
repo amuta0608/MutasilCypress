aa969167999369908dec091388993c2a54dab953
Muta <mutasila@gmail.com>
2024-07-06 00:52:40 +0700

    prettier

--- cypress/integration/common/stepDefinitions/Login.spec.ts
+++ cypress/integration/common/stepDefinitions/Login.spec.ts
@@ -1,102 +1,115 @@
 import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
-import { CustNumb, dashboard, host, password, username } from "../../../support/commands";
+import {
+  CustNumb,
+  dashboard,
+  host,
+  password,
+  username,
+} from "../../../support/commands";
 
 /**
  * Scenario login success
  */
 Given("I already on swaglab page", () => {
-    cy.visit(host);
-    cy.url().should("include", host);
+  cy.visit(host);
+  cy.url().should("include", host);
 });
-When("I click the username",()=>{
-    cy.get('[data-test="username"]').click();
+When("I click the username", () => {
+  cy.get('[data-test="username"]').click();
 });
-And("insert the username",()=>{
-    cy.get('[data-test="username"]').focus().type(username);
+And("insert the username", () => {
+  cy.get('[data-test="username"]').focus().type(username);
 });
-And("I click the password",()=>{
-    cy.get('[data-test="password"]').click();
+And("I click the password", () => {
+  cy.get('[data-test="password"]').click();
 });
-And("insert the password",()=>{
-    cy.get('[data-test="password"]').focus().type(password);
+And("insert the password", () => {
+  cy.get('[data-test="password"]').focus().type(password);
 });
-Then("click the login button",()=>{
-    cy.get('[data-test="login-button"]').click();
+Then("click the login button", () => {
+  cy.get('[data-test="login-button"]').click();
 });
-And("Directly to the dashboard page",()=>{
-    cy.url().should("include",dashboard);
+And("Directly to the dashboard page", () => {
+  cy.url().should("include", dashboard);
 
-cy.wait(1000);
-cy.get('#react-burger-menu-btn').click();
-cy.get('[data-test="logout-sidebar-link"]').click();
-cy.wait(1000);
-cy.url().should("include", host);
+  cy.wait(1000);
+  cy.get("#react-burger-menu-btn").click();
+  cy.get('[data-test="logout-sidebar-link"]').click();
+  cy.wait(1000);
+  cy.url().should("include", host);
+});
+
+/**
+ * Scenario Login Gagal Karena Username Salah
+ */
+Given("I already on swaglab page", () => {
+  cy.visit(host);
+  cy.url().should("include", host);
+});
+When("I click the username", () => {
+  cy.get('[data-test="username"]').click();
+});
+And("insert incorrect username", () => {
+  cy.get('[data-test="username"]').focus().type(CustNumb);
+});
+And("I click the password", () => {
+  cy.get('[data-test="password"]').click();
+});
+And("insert correct password", () => {
+  cy.get('[data-test="password"]').focus().type(password);
+});
+Then("Click the login button", () => {
+  cy.get('[data-test="login-button"]').click();
+});
+And("Show error message", () => {
+  cy.get('[data-test="error"]').should(
+    "have.text",
+    "Epic sadface: Username and password do not match any user in this service",
+  );
 });
 
 /**
  * Scenario Login Gagal Karena Username Salah
  */
 Given("I already on swaglab page", () => {
-    cy.visit(host);
-    cy.url().should("include", host);
+  cy.visit(host);
+  cy.url().should("include", host);
 });
-When("I click the username",()=>{
-    cy.get('[data-test="username"]').click();
+When("I click the username", () => {
+  cy.get('[data-test="username"]').click();
 });
-And("insert incorrect username",()=>{
-    cy.get('[data-test="username"]').focus().type(CustNumb);
+And("insert correct username", () => {
+  cy.get('[data-test="username"]').focus().type(username);
 });
-And("I click the password",()=>{
-    cy.get('[data-test="password"]').click();
+And("I click the password", () => {
+  cy.get('[data-test="password"]').click();
 });
-And("insert correct password",()=>{
-    cy.get('[data-test="password"]').focus().type(password);
+And("insert incorrect password", () => {
+  cy.get('[data-test="password"]').focus().type(CustNumb);
 });
-Then("Click the login button",()=>{
-    cy.get('[data-test="login-button"]').click();
+Then("Click the login button", () => {
+  cy.get('[data-test="login-button"]').click();
 });
-And("Show error message",()=>{
-    cy.get('[data-test="error"]').should('have.text','Epic sadface: Username and password do not match any user in this service');
-});
-
-
-/**
- * Scenario Login Gagal Karena Username Salah
- */
-Given("I already on swaglab page", () => {
-    cy.visit(host);
-    cy.url().should("include", host);
-});
-When("I click the username",()=>{
-    cy.get('[data-test="username"]').click();
-});
-And("insert correct username",()=>{
-    cy.get('[data-test="username"]').focus().type(username);
-});
-And("I click the password",()=>{
-    cy.get('[data-test="password"]').click();
-});
-And("insert incorrect password",()=>{
-    cy.get('[data-test="password"]').focus().type(CustNumb);
-});
-Then("Click the login button",()=>{
-    cy.get('[data-test="login-button"]').click();
-});
-And("Show error message",()=>{
-    cy.get('[data-test="error"]').should('have.text','Epic sadface: Username and password do not match any user in this service');
+And("Show error message", () => {
+  cy.get('[data-test="error"]').should(
+    "have.text",
+    "Epic sadface: Username and password do not match any user in this service",
+  );
 });
 
 /**
  * Scenario Login Gagal Karena kolom kosong
  */
 Given("I already on swaglab page", () => {
-    cy.visit(host);
-    cy.url().should("include", host);
+  cy.visit(host);
+  cy.url().should("include", host);
 });
-When("click the login button",()=>{
-    cy.get('[data-test="login-button"]').click();
+When("click the login button", () => {
+  cy.get('[data-test="login-button"]').click();
 });
-And("Show error message empty",()=>{
-    cy.get('[data-test="error"]').should("have.text","Epic sadface: Username is required");
+And("Show error message empty", () => {
+  cy.get('[data-test="error"]').should(
+    "have.text",
+    "Epic sadface: Username is required",
+  );
 });
-
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,12 +1,11 @@
 /// <reference types="cypress" />
 
-
 Cypress.on("log:added", (ev) => {
-    if (ev.displayName === "xhr" || ev.displayName === "fetch") {
-        // @ts-expect-error   --  you can modicy the types for this if you want.
-        const top$ = window.top?.Cypress.$Cypress.$;
-        top$(".command").last().hide();
-    }
+  if (ev.displayName === "xhr" || ev.displayName === "fetch") {
+    // @ts-expect-error   --  you can modicy the types for this if you want.
+    const top$ = window.top?.Cypress.$Cypress.$;
+    top$(".command").last().hide();
+  }
 });
 
 export const host = "https://www.saucedemo.com/";
@@ -15,22 +14,21 @@ export const password = "secret_sauce";
 export const dashboard = "https://www.saucedemo.com/inventory.html";
 
 export const idCy = (id: string): string => {
-    return `[data-cy="${id}"]`;
+  return `[data-cy="${id}"]`;
 };
 
-
 const generateRandomPhoneNumber = (): string => {
-    const phone = "08";
-    const digitA =
-      new Date().getFullYear().toString()[2] +
-      new Date().getFullYear().toString()[3];
-    const digitB = new Date().getMonth().toString();
-    const digitC = new Date().getDate().toString();
-    const digitD = new Date().getHours().toString();
-    const digitE = new Date().getMinutes().toString();
-    const digitF = new Date().getSeconds().toString();
-  
-    return phone + digitA + digitB + digitC + digitD + digitE + digitF;
-  };
+  const phone = "08";
+  const digitA =
+    new Date().getFullYear().toString()[2] +
+    new Date().getFullYear().toString()[3];
+  const digitB = new Date().getMonth().toString();
+  const digitC = new Date().getDate().toString();
+  const digitD = new Date().getHours().toString();
+  const digitE = new Date().getMinutes().toString();
+  const digitF = new Date().getSeconds().toString();
+
+  return phone + digitA + digitB + digitC + digitD + digitE + digitF;
+};
 
 export const CustNumb = generateRandomPhoneNumber();
