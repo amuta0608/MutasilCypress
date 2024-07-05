d4841e5b52ef6637a9113a33c2d108c4d36f3946
Muta <mutasila@gmail.com>
2024-07-06 00:51:14 +0700

    prettier

--- cypress.config.js
+++ cypress.config.js
@@ -2,46 +2,45 @@ const { defineConfig } = require("cypress");
 const browserify = require("@cypress/browserify-preprocessor");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 const resolve = require("resolve");
-  
+
 module.exports = defineConfig({
-    
-    /**
-     * Report
-     */
-    video: true,
-    screenshotOnRunFailure: true,
-    reporter: "cypress-mochawesome-reporter",
-    reporterOptions: {
-        reportPageTitle: "Labamu Desktop",
-        charts: true,
-        embeddedScreenshots: true,
-        inlineAssets: true,
-    },
-    /**
-     * Project & Options
-     */
-    projectId: "nmeysr",
-    viewportWidth: 1600,
-    viewportHeight: 1080,
-    blockHosts: ["*posthog.com"],
-    /**
-     * e2e
-     */
-    e2e: {
-        chromeWebSecurity: false,
-        setupNodeEvents(on, config) {
-            const options = {
-                ...browserify.defaultOptions,
-                typescript: resolve.sync("typescript", {
-                    baseDir: config.projectRoot,
-                }),
-            };
+  /**
+   * Report
+   */
+  video: true,
+  screenshotOnRunFailure: true,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportPageTitle: "Labamu Desktop",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+  /**
+   * Project & Options
+   */
+  projectId: "nmeysr",
+  viewportWidth: 1600,
+  viewportHeight: 1080,
+  blockHosts: ["*posthog.com"],
+  /**
+   * e2e
+   */
+  e2e: {
+    chromeWebSecurity: false,
+    setupNodeEvents(on, config) {
+      const options = {
+        ...browserify.defaultOptions,
+        typescript: resolve.sync("typescript", {
+          baseDir: config.projectRoot,
+        }),
+      };
 
-            require("cypress-mochawesome-reporter/plugin")(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
 
-            on("file:preprocessor", cucumber(options));
-        },
-        specPattern: ["cypress/integration/feature/*.feature"],
-        // specPattern: ["cypress/e2e"],
+      on("file:preprocessor", cucumber(options));
     },
+    specPattern: ["cypress/integration/feature/*.feature"],
+    // specPattern: ["cypress/e2e"],
+  },
 });
